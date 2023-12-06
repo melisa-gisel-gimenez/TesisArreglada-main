5846e1810377deb0e49348c0be363c108ae0e622
melisa-gisel-gimenez <melisag@live.com.ar>
2023-12-06 15:49:58 -0300

    mejoras form alta celulas

--- AltaLideres-C.Designer.cs
+++ AltaLideres-C.Designer.cs
@@ -55,9 +55,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 48);
+            this.label1.Location = new System.Drawing.Point(45, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.Size = new System.Drawing.Size(134, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Celulas existentes";
             // 
@@ -66,23 +66,21 @@
             this.DGVCelulas.AllowUserToAddRows = false;
             this.DGVCelulas.AllowUserToDeleteRows = false;
             this.DGVCelulas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVCelulas.Location = new System.Drawing.Point(55, 81);
-            this.DGVCelulas.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DGVCelulas.Location = new System.Drawing.Point(49, 65);
             this.DGVCelulas.Name = "DGVCelulas";
             this.DGVCelulas.ReadOnly = true;
             this.DGVCelulas.RowHeadersWidth = 51;
             this.DGVCelulas.RowTemplate.Height = 24;
-            this.DGVCelulas.Size = new System.Drawing.Size(338, 301);
+            this.DGVCelulas.Size = new System.Drawing.Size(300, 241);
             this.DGVCelulas.TabIndex = 1;
             this.DGVCelulas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVCelulas_CellContentClick);
             // 
             // btnBuscar
             // 
             this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
-            this.btnBuscar.Location = new System.Drawing.Point(415, 403);
-            this.btnBuscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBuscar.Location = new System.Drawing.Point(369, 322);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(63, 59);
+            this.btnBuscar.Size = new System.Drawing.Size(56, 47);
             this.btnBuscar.TabIndex = 2;
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
@@ -90,18 +88,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 436);
+            this.label2.Location = new System.Drawing.Point(54, 349);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(177, 20);
+            this.label2.Size = new System.Drawing.Size(150, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "DNI Lider Nueva Celula:";
             // 
             // txtDNIBuscar
             // 
-            this.txtDNIBuscar.Location = new System.Drawing.Point(253, 436);
-            this.txtDNIBuscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDNIBuscar.Location = new System.Drawing.Point(225, 349);
             this.txtDNIBuscar.Name = "txtDNIBuscar";
-            this.txtDNIBuscar.Size = new System.Drawing.Size(140, 26);
+            this.txtDNIBuscar.Size = new System.Drawing.Size(125, 22);
             this.txtDNIBuscar.TabIndex = 4;
             this.txtDNIBuscar.TextChanged += new System.EventHandler(this.txtDNIBuscar_TextChanged_1);
             this.txtDNIBuscar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDNIBuscar_KeyPress);
@@ -109,45 +106,42 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(169, 497);
+            this.label3.Location = new System.Drawing.Point(150, 398);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.Size = new System.Drawing.Size(59, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Nombre:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(169, 545);
+            this.label4.Location = new System.Drawing.Point(150, 436);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.Size = new System.Drawing.Size(60, 16);
             this.label4.TabIndex = 6;
             this.label4.Text = "Apellido:";
             // 
             // txtNombre
             // 
             this.txtNombre.Enabled = false;
-            this.txtNombre.Location = new System.Drawing.Point(253, 491);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNombre.Location = new System.Drawing.Point(225, 393);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(140, 26);
+            this.txtNombre.Size = new System.Drawing.Size(125, 22);
             this.txtNombre.TabIndex = 7;
             // 
             // txtApellido
             // 
             this.txtApellido.Enabled = false;
-            this.txtApellido.Location = new System.Drawing.Point(253, 542);
-            this.txtApellido.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtApellido.Location = new System.Drawing.Point(225, 434);
             this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(140, 26);
+            this.txtApellido.Size = new System.Drawing.Size(125, 22);
             this.txtApellido.TabIndex = 8;
             // 
             // btnAgregarCelula
             // 
-            this.btnAgregarCelula.Location = new System.Drawing.Point(463, 97);
-            this.btnAgregarCelula.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAgregarCelula.Location = new System.Drawing.Point(412, 78);
             this.btnAgregarCelula.Name = "btnAgregarCelula";
-            this.btnAgregarCelula.Size = new System.Drawing.Size(112, 50);
+            this.btnAgregarCelula.Size = new System.Drawing.Size(100, 40);
             this.btnAgregarCelula.TabIndex = 9;
             this.btnAgregarCelula.Text = "Crear Celula";
             this.btnAgregarCelula.UseVisualStyleBackColor = true;
@@ -156,10 +150,9 @@
             // btnAltaLider
             // 
             this.btnAltaLider.Enabled = false;
-            this.btnAltaLider.Location = new System.Drawing.Point(55, 600);
-            this.btnAltaLider.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAltaLider.Location = new System.Drawing.Point(49, 480);
             this.btnAltaLider.Name = "btnAltaLider";
-            this.btnAltaLider.Size = new System.Drawing.Size(184, 54);
+            this.btnAltaLider.Size = new System.Drawing.Size(164, 43);
             this.btnAltaLider.TabIndex = 10;
             this.btnAltaLider.Text = "Asignar Lider";
             this.btnAltaLider.UseVisualStyleBackColor = true;
@@ -168,29 +161,26 @@
             // txtIdCelula
             // 
             this.txtIdCelula.Enabled = false;
-            this.txtIdCelula.Location = new System.Drawing.Point(463, 171);
-            this.txtIdCelula.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtIdCelula.Location = new System.Drawing.Point(412, 137);
             this.txtIdCelula.Name = "txtIdCelula";
-            this.txtIdCelula.Size = new System.Drawing.Size(112, 26);
+            this.txtIdCelula.Size = new System.Drawing.Size(100, 22);
             this.txtIdCelula.TabIndex = 11;
             this.txtIdCelula.Visible = false;
             // 
             // txtIdMiembro
             // 
             this.txtIdMiembro.Enabled = false;
-            this.txtIdMiembro.Location = new System.Drawing.Point(463, 205);
-            this.txtIdMiembro.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtIdMiembro.Location = new System.Drawing.Point(412, 164);
             this.txtIdMiembro.Name = "txtIdMiembro";
-            this.txtIdMiembro.Size = new System.Drawing.Size(112, 26);
+            this.txtIdMiembro.Size = new System.Drawing.Size(100, 22);
             this.txtIdMiembro.TabIndex = 12;
             this.txtIdMiembro.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(55, 662);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(49, 530);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 54);
+            this.button1.Size = new System.Drawing.Size(164, 43);
             this.button1.TabIndex = 13;
             this.button1.Text = "Ver Lista Miembros por celula";
             this.button1.UseVisualStyleBackColor = true;
@@ -199,30 +189,27 @@
             // txtEtapa
             // 
             this.txtEtapa.Enabled = false;
-            this.txtEtapa.Location = new System.Drawing.Point(463, 277);
-            this.txtEtapa.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtEtapa.Location = new System.Drawing.Point(412, 222);
             this.txtEtapa.Name = "txtEtapa";
-            this.txtEtapa.Size = new System.Drawing.Size(112, 26);
+            this.txtEtapa.Size = new System.Drawing.Size(100, 22);
             this.txtEtapa.TabIndex = 14;
             this.txtEtapa.Visible = false;
             // 
             // txtRol2
             // 
             this.txtRol2.Enabled = false;
-            this.txtRol2.Location = new System.Drawing.Point(463, 311);
-            this.txtRol2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtRol2.Location = new System.Drawing.Point(412, 249);
             this.txtRol2.Name = "txtRol2";
-            this.txtRol2.Size = new System.Drawing.Size(112, 26);
+            this.txtRol2.Size = new System.Drawing.Size(100, 22);
             this.txtRol2.TabIndex = 15;
             this.txtRol2.Visible = false;
             // 
             // btnLimpiar
             // 
             this.btnLimpiar.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpiar.Image")));
-            this.btnLimpiar.Location = new System.Drawing.Point(415, 510);
-            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLimpiar.Location = new System.Drawing.Point(369, 408);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(51, 58);
+            this.btnLimpiar.Size = new System.Drawing.Size(45, 46);
             this.btnLimpiar.TabIndex = 16;
             this.btnLimpiar.UseVisualStyleBackColor = true;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
@@ -230,17 +217,18 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-4, -5);
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(648, 775);
+            this.pictureBox1.Size = new System.Drawing.Size(576, 620);
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
             // 
             // AltaCelula
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 761);
+            this.ClientSize = new System.Drawing.Size(569, 609);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.txtRol2);
             this.Controls.Add(this.txtEtapa);
@@ -260,7 +248,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AltaCelula";
             this.Text = "AltaCelula";
             this.Load += new System.EventHandler(this.AltaCelula_Load);
